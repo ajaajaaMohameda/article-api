145d90bff85e932be6e02981c65728b6669b41d4
ajaajaaMohameda <ajaajaa.mohamed@gmail.com>
2021-04-22 15:43:29 +0000

    Deserialization

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,11 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\Controller\Annotations\Post;
 use FOS\RestBundle\Controller\Annotations\View;
-
-class ArticleController
+use FOS\RestBundle\Controller\FOSRestController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+class ArticleController extends FOSRestController
 {
     /**
      * @Get(
@@ -21,4 +26,28 @@ class ArticleController
     {
         return $article;
     }
+
+    /**
+     * @Rest\Post(
+     *  path="/articles",
+     * name="app_article_create"
+     * )
+     * 
+     * @Rest\View(StatusCode=201)
+     * 
+     * @ParamConverter("article", converter="fos_rest.request_body")
+     */
+
+     public function create(Article $article)
+     {
+         $em =$this->getDoctrine()->getManager();
+         $em->persist($article);
+         $em->flush();
+
+         return $this->view($article, Response::HTTP_CREATED, [
+             'location' => $this->generateUrl('app_article_show', ['id' => $article->getId()],
+             UrlGeneratorInterface::ABSOLUTE_URL)
+         ]);
+     }
+
 }
