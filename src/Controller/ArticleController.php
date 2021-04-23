59664b97a3aab0c105708f18c5a6846df3231727
ajaajaaMohameda <ajaajaa.mohamed@gmail.com>
2021-04-23 12:00:54 +0000

    Complete crud

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,12 +9,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\Controller\Annotations\Post;
+use FOS\RestBundle\Controller\Annotations\Put;
+use FOS\RestBundle\Controller\Annotations\Delete;
 use FOS\RestBundle\Controller\Annotations\View;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use FOS\RestBundle\Request\ParamFetcherInterface;
+use Symfony\Component\Translation\Exception\InvalidResourceException;
 use Symfony\Component\Validator\ConstraintViolationList;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
@@ -31,6 +34,54 @@ class ArticleController extends AbstractFOSRestController
     public function show(Article $article)
     {
         return $article;
+    }
+
+    /**
+     * @Put(
+     *  path="/articles/{id}",
+     *  name="app_articles_update",
+     *  requirements= {"id"="\d+"}
+     * )
+     * @paramConverter("data", converter="fos_rest.request_body")
+     */
+    public function update(Article $article, Article $data, ConstraintViolationList $violations)
+    {
+
+        if (count($violations)) {
+            $message = 'the JSON sent contains invalid data. Here are the errors you need to correct: ';
+
+            foreach ($violations as $violation) {
+
+                $message .= sprintf("field %s: %s", $violation->getPropertyPath(), $violation->getMessage());
+
+                throw new InvalidResourceException($message);
+            }
+        }
+
+        $article->setTitle($data->getTitle());
+        $article->setContent($data->getContent());
+
+        $em = $this->getDoctrine()->getManager();
+        $em->flush();
+
+        return $this->view($article, Response::HTTP_OK);
+
+    }
+
+    /**
+     * @Delete(
+     *  path="/articles/{id}",
+     *  name="app_articles_delete",
+     *  requirements= {"id"="\d+"}
+     * )
+     * @Rest\View(StatusCode = 204)
+     */
+    public function delete(Article $article)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($article);
+        $em->flush();
+        return;
     }
 
     /**
